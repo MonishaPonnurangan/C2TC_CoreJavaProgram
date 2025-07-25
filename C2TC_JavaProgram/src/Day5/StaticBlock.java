6610e2218be4e99cdd06fddf864f971d703313fe
Magesh kumar <Magesh kumar@LAPTOP-0J9CGH4B>
2025-07-25 08:49:12 +0530

    Day Six Learning..

---
+++ C2TC_JavaProgram/src/Day5/StaticBlock.java
@@ -0,0 +1,15 @@
+package Day5;
+
+public class StaticBlock {
+
+	public static void main(String[] args) {
+		System.out.println("helllllooooooooooo");
+	}
+	
+	static
+	{
+		System.out.println("Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+	}
+	
+
+}
